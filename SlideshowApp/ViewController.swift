c8de3e2f9d65a80feb94fff34e149eed557cf54a
Kondoubanta <rei-016@hotmail.com>
2019-06-28 15:15:30 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 近藤万太 on 2019/06/28.
+//  Copyright © 2019 banta.kondou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
